6f6cf540c226336a84d5433398e230d417b8b02f
Pranaya Adhikari <pranayaadhikari@MacBook-Pro.local>
2017-06-05 19:00:09 -0700

    Initial Commit

---
+++ Algorithms/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Algorithms
+//
+//  Created by Pranaya Adhikari on 6/5/17.
+//  Copyright © 2017 Pranaya Adhikari. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
